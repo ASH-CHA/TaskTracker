c9f289e242a81aef63848af6b777fdeed34acef3
ASH-CHA <ashton15chavez@gmail.com>
2025-11-15 00:55:19 +0100

    Added add, delete, and update methods [Basic Structure - Needs Definition]

--- TaskTracker.java
+++ TaskTracker.java
@@ -2,7 +2,25 @@ import java.util.Scanner;
 
 public class TaskTracker {
     
+    String msg;
+    public TaskTracker(){
+        msg = new String();
+    }
 
+    public String add(String str){
+        msg += str;
+        return msg;
+    }
+
+    public String delete(String str){
+        msg.replaceAll(str, "");
+        return msg;
+    }
+
+    public void update(String str){
+        msg = str;
+    }
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
